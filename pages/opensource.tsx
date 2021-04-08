3d553e5cb40522f83d68dbd43912968c33f72f1d
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-04-08 01:51:15 +0000

    updated title of opensource page

--- pages/opensource.tsx
+++ pages/opensource.tsx
@@ -4,7 +4,7 @@ import Page from '@components/page';
 
 export default function Conf() {
   const meta = {
-    title: 'Diversity, Equity & Inclusion | DevX Conf',
+    title: 'Open-source | DevX Conf',
     description: META_DESCRIPTION
   };
 
