7d2f85bb21f6b5ada47e0b60817a4e307624f51a
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-13 22:47:53 +0500

    wip: add voting section.

---
+++ components/opensource/vote.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Section from '@components/layout/section';
+import styles from './vote.module.css';
+
+const Vote = () => (
+  <Section>
+    <div className="row">
+      <div className={styles.wrapper}>
+        <h2 className="heading-secondary">Vote for your favourite open-source project</h2>
+        <p>
+          And we’ll fund the winning project with a <span className={styles.imp}>$5k donation.</span>
+        </p>
+      </div>
+    </div>
+  </Section>
+);
+
+export default Vote;
