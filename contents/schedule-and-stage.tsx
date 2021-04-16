73ee729e58356b2f88b29bb7278b4b89e625ccbb
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-16 19:44:30 +0500

    Update individual speakers, add the contents + fixes and updates for the schedule. (#53)
    * wip.
    
    * wip.
    
    * add info regarding the individual speakers. (#51)
    
    * add info regarding the individual speakers.
    
    * update erics title.
    
    * fix the build.
    
    * add mike and and anton to the speakers list.
    
    * add the schedule for April 28th.
    
    * add the schedule for April 29.
    
    * schedule adjust the spacings.
    
    * display 4 cards in a row.
    
    * render titles only if the bio and abstract exist.
    
    * small fixes.
    
    * have the logos in grey.
    
    * fix the build.
    
    * remove the schedule page for now.
    
    * remove the un-necessary space.
    
    * update kyles description.
    
    * add rachel meyers to the speakers list.
    
    * update speaker details
    
    * update Erich Gammas profile
    
    * fix the build.
    
    * update the titles.
    
    Co-authored-by: christin <christin@gitpod.io>

--- contents/schedule-and-stage.tsx
+++ contents/schedule-and-stage.tsx
@@ -1,5 +1,9 @@
 import { Stage } from '@lib/types';
+import { isEurope } from 'utils/helpers';
 import { speakers } from 'contents';
+
+const getSpeakerByName = (name: string) =>
+  speakers.find(s => s.name.toLowerCase() === name.toLowerCase());
 
 export const allStages: Stage[] = [
   {
@@ -10,33 +14,119 @@ export const allStages: Stage[] = [
     stream: '',
     schedule: [
       {
-        title: 'Example',
-        description: 'Test',
-        start: '9:00AM',
-        end: '9:30AM',
-        speaker: speakers[0]
+        title: 'Intro',
+        description: '',
+        start: isEurope() ? '17:00' : '8:00 AM',
+        end: isEurope() ? '17:10' : '8:10 AM',
+        speaker: [
+          {
+            name: 'Johannes',
+            company: '',
+            title: '',
+            image: {
+              url: 'johannes.jpeg'
+            },
+            bio: '',
+            twitter: '',
+            linkedin: '',
+            github: '',
+            abstract: ''
+          },
+          {
+            name: 'Christin',
+            company: '',
+            title: '',
+            image: {
+              url: 'christin.jpeg'
+            },
+            bio: '',
+            twitter: '',
+            linkedin: '',
+            github: '',
+            abstract: ''
+          }
+        ],
+        isLinkLess: true
       },
       {
-        title: 'Example',
-        description: 'Test',
-        start: '9:30AM',
-        end: '10:00AM',
-        speaker: speakers[1]
+        title: 'Fireside Chat with Johannes',
+        description: '',
+        start: isEurope() ? '17:10' : '8:10 AM',
+        end: isEurope() ? '17:40' : '8:40 AM',
+        speaker: getSpeakerByName('Erich Gamma')
       },
       {
-        title: 'Example',
-        description: 'Test',
-        start: '10:00AM',
-        end: '10:30AM',
-        speaker: speakers[0]
+        title: 'Development Outage/ Treating your default branch as production',
+        description: '',
+        start: isEurope() ? '17:40' : '8:40 AM',
+        end: isEurope() ? '18:00' : '9:00 AM',
+        speaker: getSpeakerByName('Borja Burgos')
       },
       {
-        title: 'Example',
-        description: 'Test',
-        start: '9:00AM',
-        end: '9:30AM',
-        speaker: speakers[0]
-      }, 
+        title: "Is DevEx not good… because we don't want it to be?",
+        description: '',
+        start: isEurope() ? '18:00' : '9:00 AM',
+        end: isEurope() ? '18:20' : '9:20 AM',
+        speaker: getSpeakerByName('Ellen Chisa')
+      },
+      {
+        title: 'Monorepos, are they right for you?',
+        description: '',
+        start: isEurope() ? '18:20' : '9:20 AM',
+        end: isEurope() ? '18:40' : '9:40 AM',
+        speaker: getSpeakerByName('Maël Nison')
+      },
+      {
+        title: 'Fireside Chat',
+        description: '',
+        start: isEurope() ? '18:40' : '9:40 AM',
+        end: isEurope() ? '19:00' : '10:00 AM',
+        speaker: getSpeakerByName('Jason Warner')
+      },
+      {
+        title: 'Cloud Native Java',
+        description: '',
+        start: isEurope() ? '19:00' : '10:00 AM',
+        end: isEurope() ? '19:20' : '10:20 AM',
+        speaker: getSpeakerByName('Josh Long')
+      },
+      {
+        title: 'Why I love incremental delivery and why you should too.',
+        description: '',
+        start: isEurope() ? '19:20' : '10:20 AM',
+        end: isEurope() ? '19:40' : '10:40 AM',
+        speaker: getSpeakerByName('Lili Kastilio')
+      },
+      {
+        title: 'TBD',
+        description: '',
+        start: isEurope() ? '19:40' : '10:40 AM',
+        end: isEurope() ? '20:00' : '11:00 AM',
+        speaker: getSpeakerByName('Yevgeny Pats')
+      },
+      {
+        title: 'Find friction in your developer experience by user testing with developers.',
+        description: '',
+        start: isEurope() ? '20:00' : '11:00 AM',
+        end: isEurope() ? '20:20' : '11:20 AM',
+        speaker: getSpeakerByName('Ian Jennings')
+      },
+      {
+        title: 'Gitpod',
+        description: '',
+        start: isEurope() ? '20:20' : '11:20 AM',
+        end: isEurope() ? '20:40' : '11:40 AM',
+        // eslint-disable-next-line
+        // @ts-ignore
+        speaker: [getSpeakerByName('Mike Nikles'), getSpeakerByName('Anton Kosyakov')]
+      },
+      {
+        title: 'The Developer Experience Gap',
+        description: '',
+        start: isEurope() ? '20:40' : '11:40 AM',
+        end: isEurope() ? '21:00' : '12:00 PM',
+        speaker: getSpeakerByName("Stephen O'Grady")
+      }
     ]
   },
   {
@@ -47,26 +137,104 @@ export const allStages: Stage[] = [
     stream: '',
     schedule: [
       {
-        title: 'Example',
-        description: 'Test',
-        start: '9:00AM',
-        end: '9:30AM',
-        speaker: speakers[0]
+        title: 'DX, Performance, Security - Choose any three',
+        description: '',
+        start: isEurope() ? '17:00' : '8:00 AM',
+        end: isEurope() ? '17:20' : '8:20 AM',
+        speaker: getSpeakerByName("Inian Parameshwaran"),
+        isLinkLess: true
       },
       {
-        title: 'Example',
-        description: 'Test',
-        start: '9:30AM',
-        end: '10:00AM',
-        speaker: speakers[1]
+        title: 'TBD',
+        description: '',
+        start: isEurope() ? '17:20' : '8:20 AM',
+        end: isEurope() ? '17:40' : '8:40 AM',
+        speaker: {
+          name: 'TBD',
+          company: '',
+          title: '',
+          image: {
+            url: 'placeholder.svg'
+          },
+          bio: '',
+          twitter: '',
+          linkedin: '',
+          github: '',
+          abstract: ''
+        },
       },
       {
-        title: 'Example',
-        description: 'Test',
-        start: '10:00AM',
-        end: '10:30AM',
-        speaker: speakers[0]
+        title: 'DevX: History of Code Search',
+        description: '',
+        start: isEurope() ? '17:40' : '8:40 AM',
+        end: isEurope() ? '18:00' : '9:00 AM',
+        speaker: getSpeakerByName('Beyang Liu')
+      },
+      {
+        title: "The story of GitHub1s - speed & familiarity matters",
+        description: '',
+        start: isEurope() ? '18:00' : '9:00 AM',
+        end: isEurope() ? '18:20' : '9:20 AM',
+        speaker: getSpeakerByName('Yihong Cheng')
+      },
+      {
+        title: 'TBD',
+        description: '',
+        start: isEurope() ? '18:20' : '9:20 AM',
+        end: isEurope() ? '18:40' : '9:40 AM',
+        speaker: getSpeakerByName('Shanea Leven')
+      },
+      {
+        title: 'TBD',
+        description: '',
+        start: isEurope() ? '18:40' : '9:40 AM',
+        end: isEurope() ? '19:00' : '10:00 AM',
+        speaker: getSpeakerByName('Henry Zhu')
+      },
+      {
+        title: 'Simplifying the Journey of Your Containerized Application from Dev to Production',
+        description: '',
+        start: isEurope() ? '19:00' : '10:00 AM',
+        end: isEurope() ? '19:20' : '10:20 AM',
+        speaker: getSpeakerByName('Kyle Quest')
+      },
+      {
+        title: 'TBD',
+        description: '',
+        start: isEurope() ? '19:20' : '10:20 AM',
+        end: isEurope() ? '19:40' : '10:40 AM',
+        speaker: getSpeakerByName('Anurag Goel')
+      },
+      {
+        title: 'Make your APIs Developer-friendly',
+        description: '',
+        start: isEurope() ? '19:40' : '10:40 AM',
+        end: isEurope() ? '20:00' : '11:00 AM',
+        speaker: getSpeakerByName('Aidan Cunniffe')
+      },
+      {
+        title: 'How to build a dApp',
+        description: '',
+        start: isEurope() ? '20:00' : '11:00 AM',
+        end: isEurope() ? '20:20' : '11:20 AM',
+        speaker: getSpeakerByName('Nader Dabit')
+      },
+      {
+        title: '"Testing" in Prod Isn\'t the Only Way: Why Observability Needs Structure',
+        description: '',
+        start: isEurope() ? '20:20' : '11:20 AM',
+        end: isEurope() ? '20:40' : '11:40 AM',
+        speaker: getSpeakerByName('Jean Yang')
+      },
+      {
+        title: 'How to prioritize dev experience in the development process above all else',
+        description: '',
+        start: isEurope() ? '20:40' : '11:40 AM',
+        end: isEurope() ? '21:00' : '12:00 PM',
+        // eslint-disable-next-line
+        // @ts-ignore
+        speaker: [getSpeakerByName("Tuomas Artman"), getSpeakerByName("Thomas Paul Mann")]
       }
     ]
-  },
+  }
 ];
