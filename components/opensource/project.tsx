3d629286ec71ccffaca761cac5e84e8c32ea214c
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-04-23 00:52:25 +0500

    finish up the ui.

--- components/opensource/project.tsx
+++ components/opensource/project.tsx
@@ -1,7 +1,39 @@
+import IconGithub from '@components/icons/icon-github';
+import { Project as ProjectProps } from '@lib/types';
 import React from 'react';
+import cn from 'classnames';
+import styles from './project.module.css';
 
-const Project = () => (
-    <div></div>
-)
+const IconSite = () => (
+  <svg height="18" width="18" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 15 15">
+    <path
+      fill="#8E8787"
+      d="M12.593 12.927H1.574V1.908h5.51V.334h-5.51C.7.334 0 1.042 0 1.908v11.019c0 .865.7 1.574 1.574 1.574h11.019a1.58 1.58 0 001.574-1.574v-5.51h-1.574v5.51zM8.657.334v1.574h2.826L3.746 9.645l1.11 1.11 7.737-7.737v2.825h1.574V.334h-5.51z"
+    ></path>
+  </svg>
+);
 
-export default Project
+const Project = ({ logo, title, description, github, website }: ProjectProps) => (
+  <div className={styles.project}>
+    <img className={styles.logo} src={`/projects/${logo}`} alt={title} />
+    <div className={styles.description}>
+      {description}
+        <div className={styles.icons}>
+        <a href={github} target="_blank">
+        <IconGithub color="#8E8787" size={20} />
+      </a>
+      {website ? (
+        <a href={website} target="_blank">
+          <IconSite />
+        </a>
+      ) : null}
+        </div>
+    </div>
+    <div className={styles.votes}>
+        <a role="button" className={cn("btn", styles.btn)}>Vote</a>
+        <p>101 votes</p>
+    </div>
+  </div>
+);
+
+export default Project;
--- components/opensource/vote.tsx
+++ components/opensource/vote.tsx
@@ -1,5 +1,7 @@
+import Project from './project';
 import React from 'react';
 import Section from '@components/layout/section';
+import { projects } from 'contents/opensource';
 import styles from './vote.module.css';
 
 const Vote = () => (
@@ -11,6 +13,14 @@ const Vote = () => (
           And we’ll fund the winning project with a{' '}
           <span className={styles.imp}>$5k donation.</span>
         </p>
+      </div>
+      <div className={styles.projects}>
+        {
+          projects.map(project => <Project 
+            {...project}
+            key={project.title}
+          />)
+        }
       </div>
     </div>
   </Section>
