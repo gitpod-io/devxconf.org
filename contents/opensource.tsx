d0cafcfc5cdd005bdf0a6faacc34195435d08360
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-04-28 06:04:36 +0000

    updated list of projects

--- contents/opensource.tsx
+++ contents/opensource.tsx
@@ -1,6 +1,126 @@
 import { Project } from '@lib/types';
 
 export const projects: Project[] = [
+
+  {
+    title: 'ccls',
+    description: <p>
+        <strong>ccls</strong> is a C/C++/ObjC language server supporting cross references, hierarchies, completion and semantic highlighting.
+    </p>,
+    logo: 'ccls.svg',
+    github: 'https://github.com/MaskRay/ccls',
+    website: ''
+  },
+
+
+  {
+    title: 'erlang_ls',
+    description: (
+      <p>
+        <strong>erlang_ls</strong> is an implementation of Language Server Protocol for the Erlang
+        programming language.
+      </p>
+    ),
+    logo: 'erlang_ls.svg',
+    github: 'https://github.com/erlang-ls/erlang_ls',
+    website: 'https://erlang-ls.github.io/'
+  },
+
+
+  {
+    title: 'ElixirLS',
+    description: <p>
+        <strong>ElixirLS</strong> is a frontend-independent IDE "smartness" server for Elixir. Implements the "Language Server Protocol" standard and provides debugger support via the "Debug Adapter Protocol"
+    </p>,
+    logo: 'elixirls.svg',
+    github: 'https://github.com/elixir-lsp/elixir-ls',
+  },
+
+  {
+    title: 'elm-language-server',
+    description: (
+      <p>
+        <strong>elm-language-server</strong> is an implementation of Language Server Protocol for the ELM
+        programming language.
+      </p>
+    ),
+    logo: 'elm-language-server.svg',
+    github: 'https://github.com/elm-tooling/elm-language-server',
+    website: ''
+  },
+
+  {
+    title: 'hadolint',
+    description: (
+      <p>
+        <strong>hadolint</strong> is a smarter Dockerfile linter that helps you build best practice Docker images. The linter is parsing the Dockerfile into an AST and performs rules on top of the AST. It is standing on the shoulders of ShellCheck to lint the Bash code inside RUN instructions.
+      </p>
+    ),
+    logo: 'hadolint.svg',
+    github: 'https://github.com/hadolint/hadolint',
+    website: ''
+  },
+
+  {
+    title: 'homebrew',
+    description: (
+      <p>
+        <strong>homebrew</strong> is the missing package manager for macOS (or Linux).
+      </p>
+    ),
+    logo: 'homebrew.svg',
+    github: 'https://github.com/Homebrew/brew',
+    website: 'https://brew.sh/'
+  },
+
+
+  {
+    title: 'Haskell IDE Engine',
+    description: (
+      <p>
+        <strong>Haskell IDE Engine</strong> aims to be the universal interface to a growing number of Haskell tools,
+        providing a fully-featured Language Server Protocol server for editors and IDEs that require
+        Haskell-specific functionality.
+      </p>
+    ),
+    logo: 'haskell-ide-engine.svg',
+    github: 'https://github.com/haskell/haskell-ide-engine',
+  },
+
+  {
+    title: 'Ionide',
+    description: <p>
+        <strong>Ionide</strong> is an organization which builds high quality, cross platform developer tools for F#. Our flagship project is Ionide-VSCode - a Visual Studio Code plugin, with more than 1 million downloads, transforming VSCode into a fully-fledged F# IDE
+    </p>,
+    logo: 'ionide.svg',
+    github: 'https://github.com/ionide',
+    website: 'https://ionide.io/'
+  },
+
+  {
+    title: 'lsp-mode',
+    description: (
+      <p>
+        <strong>lsp-mode</strong> is a Emacs client/library for the Language Server Protocol with multiples languages support.
+      </p>
+    ),
+    logo: 'lsp-mode.svg',
+    github: 'https://github.com/emacs-lsp/lsp-mode',
+    website: 'https://emacs-lsp.github.io/lsp-mode/'
+  },
+
+  {
+    title: 'ruby-together',
+    description: (
+      <p>
+        <strong>ruby-together</strong> is a grassroots initiative committed to supporting the critical Ruby infrastructure you rely on: Bundler, RubyGems, and other shared tools.
+      </p>
+    ),
+    logo: 'ruby-together.svg',
+    github: '',
+    website: 'https://rubytogether.org/'
+  },
+
   {
     title: 'Rust-analyzer',
     description: (
@@ -14,33 +134,90 @@ export const projects: Project[] = [
     github: 'https://github.com/rust-analyzer/rust-analyzer',
     website: 'https://rust-analyzer.github.io/'
   },
+
   {
-    title: 'Haskell IDE Engine',
+    title: 'rvm',
     description: (
       <p>
-        <strong>Haskell IDE Engine</strong> aims to be the universal interface to a growing number of Haskell tools,
-        providing a fully-featured Language Server Protocol server for editors and IDEs that require
-        Haskell-specific functionality.
+        <strong>rvm</strong> is the acronym of Ruby enVironment Manager. It manages Ruby application environments and enables switching between them..
       </p>
     ),
-    logo: 'haskell-ide-engine.svg',
-    github: 'https://github.com/haskell/haskell-ide-engine',
+    logo: 'rvm.svg',
+    github: 'https://github.com/rvm/rvm',
+    website: 'https://rvm.io/'
   },
+
+
   {
-    title: 'ElixirLS',
+    title: 'shellcheck',
     description: <p>
-        <strong>ElixirLS</strong> is a frontend-independent IDE "smartness" server for Elixir. Implements the "Language Server Protocol" standard and provides debugger support via the "Debug Adapter Protocol"
+        <strong>shellcheck</strong> is a tool that gives warnings and suggestions for bash/sh shell scripts:
     </p>,
-    logo: 'elixirls.svg',
-    github: 'https://github.com/elixir-lsp/elixir-ls',
+    logo: 'shellcheck.svg',
+    github: 'https://github.com/koalaman/shellcheck',
+    website: ''
   },
+
+
   {
-    title: 'Ionide',
+    title: 'patch-package',
+    description: (
+      <p>
+        <strong>patch-package</strong> lets app authors instantly make and keep fixes to npm dependencies. A vital band-aid for javascript developers living on the bleeding edge.
+      </p>
+    ),
+    logo: 'patch-package.svg',
+    github: 'https://github.com/ds300/patch-package',
+    website: ''
+  },
+
+  {
+    title: 'php-language-server',
+    description: (
+      <p>
+        <strong>php-language-server</strong> is an implementation of Language Server Protocol for the PHP
+        programming language.
+      </p>
+    ),
+    logo: 'php-language-server.svg',
+    github: 'https://github.com/felixfbecker/php-language-server',
+    website: ''
+  },
+
+  {
+    title: 'pre-commit',
+    description: (
+      <p>
+        <strong>pre-commit</strong> is a framework for managing and maintaining multi-language pre-commit git hooks.
+      </p>
+    ),
+    logo: 'pre-commit.svg',
+    github: 'https://github.com/pre-commit/pre-commit',
+    website: 'https://pre-commit.com/'
+  },
+
+  {
+    title: 'vim-lsp',
     description: <p>
-        <strong>Ionide</strong> is an organization which builds high quality, cross platform developer tools for F#. Our flagship project is Ionide-VSCode - a Visual Studio Code plugin, with more than 1 million downloads, transforming VSCode into a fully-fledged F# IDE
+        <strong>vim-lsp</strong> is an async Language Server Protocol plugin for vim8 and neovim.
     </p>,
-    logo: 'ionide.svg',
-    github: 'https://github.com/ionide',
-    website: 'https://ionide.io/'
-  }
-];
+    logo: 'vim-lsp.svg',
+    github: 'https://github.com/prabirshrestha/vim-lsp',
+    website: ''
+  },
+
+  {
+    title: 'yamllint',
+    description: (
+      <p>
+        <strong>yamllint</strong> is a linter for YAML.
+      </p>
+    ),
+    logo: 'yamllint.svg',
+    github: 'https://github.com/adrienverge/yamllint',
+    website: ''
+  },
+
+
+ 
+ ];
