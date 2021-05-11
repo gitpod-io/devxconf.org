c091b0afdf4896f8a3d6ae53e768d7cf814e7de2
nisarhassan12 <syednisarhassan12@gmail.com>
2021-05-11 22:08:41 +0500

    re-add speakers.

---
+++ pages/speakers/index.tsx
@@ -0,0 +1,22 @@
+import Layout from '@components/layout';
+import { META_DESCRIPTION } from '@lib/constants';
+import Page from '@components/page';
+import SpeakersGrid from '@components/speakers-grid';
+import { speakers } from 'contents';
+
+const SpeakersPage = () => {
+  const meta = {
+    title: 'Speakers | DevX Conf',
+    description: META_DESCRIPTION
+  };
+
+  return (
+    <Page meta={meta}>
+      <Layout>
+        <SpeakersGrid speakers={speakers} />
+      </Layout>
+    </Page>
+  );
+};
+
+export default SpeakersPage;
