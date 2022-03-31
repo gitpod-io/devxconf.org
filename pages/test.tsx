b5b1f36c565abd3ea05586cf46507c231dbe594f
nisarhassan12 <syednisarhassan12@gmail.com>
2022-03-31 16:23:04 +0500

    remove the test page.

--- pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import GitHubButton from "@components/index/github-button";
-import Page from "@components/page";
-import React from "react";
-
-export default () => (
-	<Page meta={{title: "", description: ""}}>
-		<GitHubButton text="Register with" />
-	</Page>
-)
