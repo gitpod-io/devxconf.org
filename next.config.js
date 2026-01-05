77e93d9f49de61bba77100b2418d6a595d64d121
Siddhant Khare <Siddhantkhare2694@gmail.com>
2026-01-05 10:13:06 +0000

    Use Netlify _redirects file for redirect
    Next.js redirects don't work with Netlify plugin. Using _redirects file instead.
    
    Co-authored-by: Ona <no-reply@ona.com>

--- next.config.js
+++ next.config.js
@@ -16,15 +16,6 @@
 
 module.exports = {
   target: 'serverless',
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://ona.com',
-        permanent: false,
-      },
-    ];
-  },
   images: {
     domains: [
       'www.datocms-assets.com',
